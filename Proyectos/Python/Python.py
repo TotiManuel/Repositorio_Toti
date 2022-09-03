95684fb29f6e9c04063c8c9624716c62cdd2f54d
TotiManuel <65439298+TotiManuel@users.noreply.github.com>
2022-09-03 06:37:17 +0000

    Update and rename 1 to Python.py

---
+++ Proyectos/Python/Python.py
@@ -0,0 +1,5 @@
+Numero = 1 #Asigno el valor de 1 a la variable Numero
+ciudad = "Villa Maria" #Asigno la String de "Villa Maria" a la variable de ciudad
+print("Hola " + ciudad) #Imprimo en consola "Hola " + el valor de la variable ciudad(Villa Maria)
+print(Numero) #Imprimo en consola el valor de la variable Numero(1)
+print(type(9)) #Imprimo en consola que tipo de dato es el valor. (9=int)
